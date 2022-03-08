1445af0e2d5a392e065d4204b47d96f61dda1ede
BaranovaElena <baranovaeaa@yandex.ru>
2022-03-08 16:44:19 +0300

    add settings to app bar

---
+++ app/src/main/java/com/example/shoplist/SettingsFragment.kt
@@ -0,0 +1,9 @@
+package com.example.shoplist
+
+import androidx.fragment.app.Fragment
+
+class SettingsFragment : Fragment(R.layout.fragment_settings) {
+    companion object {
+        fun newInstance() = SettingsFragment()
+    }
+}
--- app/src/main/java/com/example/shoplist/ui/MainActivity.kt
+++ app/src/main/java/com/example/shoplist/ui/MainActivity.kt
@@ -2,15 +2,16 @@ package com.example.shoplist.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.shoplist.R
+import com.example.shoplist.SettingsFragment
 import com.example.shoplist.databinding.ActivityMainBinding
 import com.example.shoplist.ui.favorites.FavoritesFragment
 import com.example.shoplist.ui.recipe.RecipesFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
-
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     private val binding by viewBinding(ActivityMainBinding::bind , R.id.activity_container)
@@ -25,17 +26,38 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     private fun setNavigation(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.nav_shop_list -> openFragment(ShoplistFragment.newInstance())
-            R.id.nav_recipes -> openFragment(RecipesFragment.newInstance())
-            R.id.nav_favorites -> openFragment(FavoritesFragment.newInstance())
+            R.id.nav_shop_list -> openFragment(ShoplistFragment.newInstance(), false)
+            R.id.nav_recipes -> openFragment(RecipesFragment.newInstance(), false)
+            R.id.nav_favorites -> openFragment(FavoritesFragment.newInstance(), false)
         }
         return true
     }
 
-    private fun openFragment(fragmentInstance: Fragment) {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.container, fragmentInstance)
-            .commitNow()
+    private fun openFragment(fragmentInstance: Fragment, backstack: Boolean) {
+        if (backstack) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, fragmentInstance)
+                .addToBackStack(null)
+                .commit()
+        } else {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, fragmentInstance)
+                .commitNow()
+        }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.app_bar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.app_bar_settings -> {
+                openFragment(SettingsFragment.newInstance(), true)
+                true
+            }
+            else -> false
+        }
+    }
 }
